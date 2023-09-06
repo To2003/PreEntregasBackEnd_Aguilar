323e9743160ca44b2a0a9c3e2dde572d79ac7fed
To2003 <soytomasaguilar@gmail.com>
2023-09-06 20:02:06 -0300

    Problema archivos solucionado

---
+++ PreEntrega2_Aguilar/src/index.js
@@ -0,0 +1,28 @@
+import express from "express"
+import productsRoutes from './routes/products.routes.js'
+import cartsRoutes from './routes/carts.routes.js'
+import viewsRoutes from './routes/views.routes.js'
+import __dirname from "./utils.js"
+import handlebars from "express-handlebars"
+import { connectMongoDB } from "./database.js"
+
+const app = express()
+const PORT = 8080
+
+app.engine("handlebars", handlebars.engine())
+app.set("views", __dirname+"/views")
+app.set("view engine", "handlebars")
+app.use(express.static(__dirname+"/public"))
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+
+app.use("/api/products", productsRoutes)
+app.use("/api/carts", cartsRoutes)
+app.use("/", viewsRoutes)
+
+app.listen(PORT, () =>{
+    console.log(`server running at port ${PORT}`);
+})
+
+connectMongoDB()
